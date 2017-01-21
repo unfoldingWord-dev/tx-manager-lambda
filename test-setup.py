f6b5908867d79791905595e93c72c3600e52aede
Richard Mahn <richard_mahn@wycliffeassociates.org>
2017-01-21 11:46:30 -0700

    Initial commit

---
+++ test-setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="tx-manager",
+    version="0.0.1",
+    author="unfoldingWord",
+    author_email="unfoldingword.org",
+    description="Unit test setup file.",
+    keywords="",
+    url="https://github.org/unfoldingWord-dev/tx-manager",
+    packages=['tx_manager'],
+    long_description='Unit test setup file',
+    classifiers=[],
+    install_requires=[
+        'tx-manager'
+    ],
+    test_suite='tests'
+)
