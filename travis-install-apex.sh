2b505ffc9e568d6d46c47af206117a390cfe7b28
Richard Mahn <richard_mahn@wycliffeassociates.org>
2017-06-01 02:20:25 +0800

    Make sure there is always a LATEST for apex install

--- travis-install-apex.sh
+++ travis-install-apex.sh
@@ -4,6 +4,9 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PARENT_DIR="$( dirname "${THIS_DIR}" )"
 
 LATEST=$(curl -s https://api.github.com/repos/apex/apex/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
+if [ -z $LATEST ]; then
+    LATEST=v0.14.0
+fi
 URL="https://github.com/apex/apex/releases/download/${LATEST}/apex_linux_amd64"
 DEST="${PARENT_DIR}/apex"
 
